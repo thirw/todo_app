1d7bb4f300424ecd28f3ed508776d593d356c789
Thirawat Prasert <tpr@manaosoftware.com>
2021-10-06 23:28:17 +0700

    Edit model and delete unused data

--- lib/data/todo_model.dart
+++ lib/data/todo_model.dart
@@ -1,5 +1,13 @@
 class TodoModel {
-  late String name;
-  late bool checked;
-  TodoModel(this.name, this.checked);
+  String? name;
+  bool? checked;
+
+  TodoModel({this.name, this.checked});
+
+  TodoModel.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        checked = json['checked'];
+
+  Map<String, dynamic> toJson() => {'name': name, 'checked': checked};
+
 }
